1ecf645ce40887397df39a1ce8ea92c4e72f8af4
Brian Niedzialkoski <brianniedzialkoski@gmail.com>
2017-11-24 15:47:07 -0500

    Add functional ExoPlayer for phones

--- app/src/main/java/com/popularpenguin/bakingapp/InstructionsFragment.java
+++ app/src/main/java/com/popularpenguin/bakingapp/InstructionsFragment.java
@@ -1,9 +1,16 @@
 package com.popularpenguin.bakingapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.session.MediaButtonReceiver;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,16 +19,38 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.popularpenguin.bakingapp.Data.Recipe;
+import com.popularpenguin.bakingapp.Data.Step;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /** The fragment holding the individual instructions for a step */
-public class InstructionsFragment extends Fragment implements View.OnClickListener {
+public class InstructionsFragment extends Fragment implements View.OnClickListener,
+        ExoPlayer.EventListener {
 
     private static final String TAG = InstructionsFragment.class.getSimpleName();
 
+    @BindView(R.id.exo_view) SimpleExoPlayerView mPlayerView;
     @BindView(R.id.tv_instructions) TextView mInstructions;
     Button mPrevious;
     Button mNext;
@@ -30,6 +59,11 @@ public class InstructionsFragment extends Fragment implements View.OnClickListen
     private int mIndex;
     private String mInstructionsText;
     private boolean isPhone;
+
+    // ExoPlayer members
+    private SimpleExoPlayer mExoPlayer;
+    private static MediaSessionCompat mMediaSession;
+    private PlaybackStateCompat.Builder mStateBuilder;
 
     public static InstructionsFragment newInstance(@NonNull Bundle args) {
         InstructionsFragment fragment = new InstructionsFragment();
@@ -44,6 +78,7 @@ public class InstructionsFragment extends Fragment implements View.OnClickListen
                              @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_instructions, container, false);
+
 
         isPhone = getResources().getBoolean(R.bool.isPhone);
 
@@ -66,11 +101,30 @@ public class InstructionsFragment extends Fragment implements View.OnClickListen
         return view;
     }
 
+    /** Get the recipe step and pass the uri to initialize ExoPlayer */
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Step step = mRecipe.getSteps().get(mIndex);
+        Uri uri = getVideoUri(step);
+
+        initMediaSession();
+        initPlayer(uri);
+    }
+
     public void setData(@NonNull Bundle args) {
         mRecipe = args.getParcelable(MainActivity.RECIPE_EXTRA);
         mIndex = args.getInt(RecipeActivity.INDEX_EXTRA);
 
         setViews();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+        mMediaSession.setActive(false);
     }
 
     /** set the text for the instructions and if it is a phone, enable/disable buttons based
@@ -84,6 +138,68 @@ public class InstructionsFragment extends Fragment implements View.OnClickListen
         if (isPhone) {
             mPrevious.setEnabled(mIndex > 0);
             mNext.setEnabled(mIndex < mRecipe.getSteps().size() -1);
+        }
+    }
+
+    /** Get the video's Uri from whatever JSON text field has it */
+    private Uri getVideoUri(Step step) {
+        if (!step.getVideoURL().isEmpty()) {
+            return Uri.parse(step.getVideoURL());
+        }
+        else {
+            // if the thumbnail is empty too just return an empty Uri
+            return Uri.parse(step.getThumbnailURL());
+        }
+    }
+
+    private void initMediaSession() {
+        mMediaSession = new MediaSessionCompat(getContext(), TAG);
+
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // MediaButtons's can't restart the player when app isn't visible
+        mMediaSession.setMediaButtonReceiver(null);
+
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+
+        mMediaSession.setCallback(new SessionCallback());
+
+        mMediaSession.setActive(true);
+    }
+
+    /** Release ExoPlayer */
+    private void releasePlayer() {
+        //mNotificationManager.cancelAll();
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+    /** Initialize ExoPlayer */
+    private void initPlayer(Uri uri) {
+        if (mExoPlayer == null) {
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+
+            mExoPlayer.addListener(this);
+
+            // prepare the MediaSource
+            String userAgent = Util.getUserAgent(getContext(), "BakingApp");
+            MediaSource mediaSource = new ExtractorMediaSource(uri,
+                    new DefaultDataSourceFactory(getContext(), userAgent),
+                    new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
         }
     }
 
@@ -109,6 +225,80 @@ public class InstructionsFragment extends Fragment implements View.OnClickListen
 
             default:
                 throw new UnsupportedOperationException("Invalid view id");
+        }
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if((playbackState == Player.STATE_READY) && playWhenReady) {
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
+                    mExoPlayer.getCurrentPosition(),
+                    1f);
+        }
+        else if (playbackState == Player.STATE_READY) {
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
+                    mExoPlayer.getCurrentPosition(),
+                    1f);
+        }
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+        //showNotification(mStateBuilder.build());
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    /** Media Session callbacks */
+    private class SessionCallback extends MediaSessionCompat.Callback {
+        @Override
+        public void onPlay() {
+            mExoPlayer.setPlayWhenReady(true);
+        }
+
+        public void onPause() {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+    }
+
+    /** Broadcast receiver */
+    public static class MediaReceiver extends BroadcastReceiver {
+
+        public MediaReceiver() { }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            MediaButtonReceiver.handleIntent(mMediaSession, intent);
         }
     }
 }
